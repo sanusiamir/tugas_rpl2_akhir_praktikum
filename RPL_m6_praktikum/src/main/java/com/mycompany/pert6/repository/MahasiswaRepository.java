7bd213a931bb59cd97837a8e4159f0f12ac6ad7f
Muhammad sanusi amir baquni <42158228+sanusiamir@users.noreply.github.com>
2025-12-04 19:34:30 +0700

    Add files via upload

---
+++ RPL_m6_praktikum/src/main/java/com/mycompany/pert6/repository/MahasiswaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.pert6.repository;
+
+import com.mycompany.pert6.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Abay
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+    
+}
