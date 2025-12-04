ce2949dbd2d72bc9c1e2937d721c5dd12e106bc9
Muhammad sanusi amir baquni <42158228+sanusiamir@users.noreply.github.com>
2025-12-04 19:28:26 +0700

    Add files via upload

---
+++ RPL_m1_praktikum/src/main/java/com/mycompany/peretemuan1_rpl2/Peretemuan1_rpl2.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peretemuan1_rpl2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Abay
+ */
+public class Peretemuan1_rpl2 {
+
+    public static void main(String[] args) {
+        //1
+//        int a = 5;
+//        int b = 10;
+//        int hasil = a+b;
+//        System.out.println("hasil akhirnya adalah : " +hasil);
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama anda: ");
+        String nama = input.nextLine();
+
+        System.out.println("Halo, " + nama);
+//        
+        //3
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("Angka ke-" + i);
+//        }
+        //4
+//           int angka = 3;
+//           
+//           if(angka > 0) {
+//               System.out.println("Angka positif");
+//           } else if (angka <0) {
+//               System.out.println("Angka negatif");
+//           } else {
+//               System.out.println("Angka nol");
+//               
+//           }
+
+
+        
+    }
+}
